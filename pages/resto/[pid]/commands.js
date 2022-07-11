0e646e191d350d202778ea60cc3d97c1b409aaed
StreaQr <streaqr@gmail.com>
2022-07-12 01:30:39 +0300

    fixes

--- pages/resto/[pid]/commands.js
+++ pages/resto/[pid]/commands.js
@@ -12,7 +12,7 @@ import { useState, useEffect } from 'react';
 import styles from '../../../styles/Commands.module.css'
 
 //modals
-const PopUpWindow = dynamic(import("components/utils/popUpWindow.js"))
+const PopUpWindow = dynamic(import("../../../components/utils/PopUpWindow.js"))
 
 // import Swiper core and required modules
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -286,7 +286,7 @@ export async function getStaticProps(context) {
 export async function getStaticPaths() {
     // We'll pre-render only these paths at build time.
     return {
-        paths: [{ params: { pid: 'palms' } }], fallback: 'blocking'
+        paths: [{ params: { pid: 'd' } }], fallback: 'blocking'
     }
 }
 
